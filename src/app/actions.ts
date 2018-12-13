f3438b30174aec8560aadf24cac50191dc07a60b
hmiranda <helio@mentel.com>
2018-12-12 19:15:03 -0500

    Project setup and started implementing

---
+++ src/app/actions.ts
@@ -0,0 +1,6 @@
+export enum ETodoActions {
+    ADD = '[TODO] add',
+    TOGGLE = '[TODO] toggle',
+    REMOVE = '[TODO] remove',
+    REMOVE_ALL = '[TODO] remove all'  
+};
---
+++ src/app/store.ts
@@ -0,0 +1,49 @@
+import { ITodo } from './models/todo.model';
+import { ETodoActions } from './actions'
+
+// All states
+export interface IAppState {
+    todos: ITodo[];
+    lastUpdate: Date;
+}
+
+export const INITIAL_STATE: IAppState = {
+    todos: [],
+    lastUpdate: null
+}
+
+// My reducer (state = 'oldState', action = 'the dispatched action')
+export function rootReducer(state, action) {
+    switch(action.type) {
+        case ETodoActions.ADD:
+            action.todo.id = state.todos.lenght + 1;
+            return Object.assign({}, state, {
+                todos: state.todos.concat(Object.assign({}, action.todo)),
+                lastUpdate: new Date()
+            });
+            
+        case ETodoActions.TOGGLE:
+            var todo: ITodo = state.todos.find(todo => todo.id === action.id);
+            var index: number = state.todos.indexOf(todo);
+            return Object.assign({}, state, {
+                todo: [
+                    ...state.todos.slice(0, index),
+                    Object.assign({}, todo, {isCompleted: !todo.isCompleted}),
+                    ...state.todos.slice(index + 1)
+                ],
+                lastUpdate: new Date()
+            });
+        case ETodoActions.REMOVE:
+            return Object.assign({}, state, {
+                todos: state.todos.filter(todo => todo.id !== action.id),
+                lastUpdate: new Date()
+            });
+        case ETodoActions.REMOVE_ALL:
+            return Object.assign({}, state, {
+                todos: [],
+                lastUpdate: new Date()
+            });
+        default:
+            return state;        
+    }
+}
